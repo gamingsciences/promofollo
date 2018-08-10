9f5cf060df3cfa273ba0a40041800b1d58a4b42e
Ken Lathrop <gamingsciences@gmail.com>
2018-08-10 14:47:59 -0700

    Remove view promo from admin

--- src/promotions/admin.py
+++ src/promotions/admin.py
@@ -15,15 +15,6 @@ class PromotionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'casino', 'url_',)
     list_display_links = ('title',)
     raw_id_fields = ('checker_runtime',)
-    readonly_fields = ['promo_image']
-    
-    def promo_image(self, obj):
-        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url = obj.image.url,
-            width=obj.image.width,
-            height=obj.image.height,
-            )
-    )
     
     def url_(self, instance):
         return format_html('<a href="{url}" target="_blank">{title}</a>'.format(
